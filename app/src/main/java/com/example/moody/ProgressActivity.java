138587f47995e01ec99cbb179248da0f1377ad7e
BogdanEmilian <emilian.bogdan02@e-uvt.ro>
2022-06-02 01:51:46 +0300

    Line graph update

--- app/src/main/java/com/example/moody/ProgressActivity.java
+++ app/src/main/java/com/example/moody/ProgressActivity.java
@@ -5,9 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import com.google.firebase.analytics.FirebaseAnalytics;
-
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 import java.util.ArrayList;
 
 
@@ -19,6 +19,24 @@ public class ProgressActivity extends AppCompatActivity {
         setContentView(R.layout.activity_progress);
 
         Button backButton = (Button) findViewById(R.id.BUTTON_goBack);
+        Button restButton = (Button) findViewById(R.id.BUTTON_Rest);
+        GraphView graph = findViewById(R.id.graph);
+
+        DBHandler dbHandler = new DBHandler(ProgressActivity.this);
+        ArrayList<Data> array = dbHandler.getLastWeek();
+
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
+                new DataPoint(0, array.get(0).getHappiness()),
+                new DataPoint(1, array.get(1).getHappiness()),
+                new DataPoint(2, array.get(2).getHappiness()),
+                new DataPoint(3, array.get(3).getHappiness()),
+                new DataPoint(4, array.get(4).getHappiness()),
+                new DataPoint(5, array.get(5).getHappiness()),
+                new DataPoint(6, array.get(6).getHappiness())
+        });
+
+        graph.addSeries(series);
+
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,10 +45,40 @@ public class ProgressActivity extends AppCompatActivity {
             }
         });
 
-        DBHandler dbHandler = new DBHandler(ProgressActivity.this);
+        restButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (restButton.getText() == "Rest level") {
+                    graph.removeAllSeries();
+                    LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
+                            new DataPoint(0, array.get(0).getRest()),
+                            new DataPoint(1, array.get(1).getRest()),
+                            new DataPoint(2, array.get(2).getRest()),
+                            new DataPoint(3, array.get(3).getRest()),
+                            new DataPoint(4, array.get(4).getRest()),
+                            new DataPoint(5, array.get(5).getRest()),
+                            new DataPoint(6, array.get(6).getRest())
+                    });
 
-        ArrayList<Data> array = dbHandler.getLastWeek();
+                    graph.addSeries(series);
+                    restButton.setText("Happiness level");
+                } else {
+                    graph.removeAllSeries();
+                    LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
+                            new DataPoint(0, array.get(0).getHappiness()),
+                            new DataPoint(1, array.get(1).getHappiness()),
+                            new DataPoint(2, array.get(2).getHappiness()),
+                            new DataPoint(3, array.get(3).getHappiness()),
+                            new DataPoint(4, array.get(4).getHappiness()),
+                            new DataPoint(5, array.get(5).getHappiness()),
+                            new DataPoint(6, array.get(6).getHappiness())
+                    });
 
-        System.out.println("\n\n================\n"+array.get(0).getHappiness()+"\n"+array.get(0).getRest()+"\n================\n\n");
+                    graph.addSeries(series);
+                    restButton.setText("Rest level");
+                }
+            }
+        });
+
     }
 }
